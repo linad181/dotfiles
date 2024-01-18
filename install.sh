385c2fbc89b31b17c45371a2cb4f5eccfb244e94
linad181 <fptrekie1701@gmail.com>
2024-01-17 20:14:17 -0800

    Update install.sh
    Update Cockpit install behavior.

--- install.sh
+++ install.sh
@@ -18,16 +18,25 @@ else
 fi
 
 echo "Installing additional programs..."
-sudo apt update && sudo apt -y install neofetch apt-clone curl cockpit
-echo -en '\n'
-echo -en '\n'
+sudo apt -y install neofetch apt-clone curl cockpit
+echo -en '\n\n'
 
-echo "Installing 45Drives Navigator plugin for Cockpit..."
-curl -sSL https://repo.45drives.com/setup -o setup-repo.sh
-sudo bash setup-repo.sh
-sudo apt update && sudo apt install cockpit-navigator -y
-echo -en '\n'
-echo -en '\n'
+addCockpit () {
+    echo "Installing cockpit with 45Drives Navigator add-on..."
+    sudo apt -y install cockpit
+    echo -en '\n'
+    echo "Installing 45Drives Navigator plugin for Cockpit..."
+    curl -sSL https://repo.45drives.com/setup -o setup-repo.sh
+    sudo bash setup-repo.sh
+    sudo apt update && sudo apt install cockpit-navigator -y
+    echo -en '\n\n'
+
+    read -n1 -p "Do you want to add Docker support to Cockpit? [Y,n]" doit 
+    case $doit in  
+      y|Y) addDocker ;; 
+      n|N) echo -en '\n\n' ;; 
+    esac
+}
 
 addDocker () {
     echo "Installing Docker extension to Cockpit..."
@@ -35,20 +44,18 @@ addDocker () {
     sudo mv cockpit-docker.tar.gz /usr/share/cockpit
     sudo tar xf /usr/share/cockpit/cockpit-docker.tar.gz -C /usr/share/cockpit/
     sudo rm /usr/share/cockpit/cockpit-docker.tar.gz
-    echo -en '\n'
-    echo -en '\n'
+    echo -en '\n\n'
 }
 
-read -n1 -p "Do you want to add Docker support to Cockpit? [Y,n]" doit 
-case $doit in  
-  y|Y) addDocker ;; 
-  n|N) echo no ;; 
+read -n1 -p "Do you want to add Cockpit to this server? [Y/n]" doit
+case $doit in
+  y|Y) addCockpit ;;
+  n|N) echo -en '\n\n' ;;
 esac
 
 echo "Installing SSH keys..."
 mkdir ~/.ssh
 curl https://github.com/linad181.keys -o ~/.ssh/authorized_keys
-echo -en '\n'
-echo -en '\n'
+echo -en '\n\n'
 
 echo "Install finished."
